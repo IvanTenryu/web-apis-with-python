bd8b451d47dae610867d4e3dbc789367df9249fc
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2022-08-17 12:57:14 -0500

    No changes

--- p1-hello-api-flask/app.py
+++ p1-hello-api-flask/app.py
@@ -18,18 +18,5 @@ def index ():
   5. If both names are provided: respond with a question,
   "Is your name <fist-name> <second-name>
   """
-  fname = request.args.get("fname")
-  lname = request.args.get("lname")
-
-  if not fname and not lname:
-    # If both first name and last name are missing, return an error
-    return jsonify({ "status" : "error" })
-  elif fname and not lname:
-    # If first name is present but last name is missing
-    response = {"data":f"Hello, {fname}"}
-  elif not fname and lname:
-    # If first name is missing but last name is present
-    response = {"data":f"Hello, Mr. {lname}!"}
-  else:
-    response = {"data":f"Is your name, {fname} {lname}?" }
-  return jsonify(response)
+  
+  return "Hello, world!"
